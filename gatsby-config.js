677edcdb2a32d363186661abaa5785c523b7271a
Nnaemezie Juliet <ogochukwu.nnaemezie.247214@unn.edu.ng>
2025-09-20 08:46:58 +0100

    chore: personalized portfolio with Juliet’s info

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Juliet Nnaemezie',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Juliet Nnaemezie is a Flutter developer and final-year Electronic & Computer Engineering student, passionate about building clean, functional mobile apps that solve real problems.',
+    siteUrl: 'https://pheunixx.github.io/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@ogochukwumerem_',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Juliet Nnaemezie',
+        short_name: 'Juliet',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
@@ -152,7 +152,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-45666519-2',
+        trackingId: ,
       },
     },
   ],
